6afa94f74290150c8848d9a67ba92c1b66a192a3
Parag Naikade <paragnaikade@gmail.com>
2025-07-21 19:58:01 +1000

    added scalar api example (#9)
    Co-authored-by: Parag Naikade <parag.naikade@standards.org.au>

---
+++ OrderTrackingApp.Api/Examples/CreateOrderRequestExample.cs
@@ -0,0 +1,28 @@
+﻿using OrderTrackingApp.Api.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace OrderTrackingApp.Api.Examples;
+
+public class CreateOrderRequestExample : IExamplesProvider<CreateOrderRequest>
+{
+    public CreateOrderRequest GetExamples()
+    {
+        return new CreateOrderRequest
+        {
+            CustomerId = Guid.NewGuid(),
+            Items = new List<CreateOrderItemRequest>
+            {
+                new CreateOrderItemRequest
+                {
+                    ProductId = Guid.Parse("b111a4dd-f45c-4d40-a6a3-3002f62f823f"),
+                    Quantity = 2
+                },
+                new CreateOrderItemRequest
+                {
+                    ProductId = Guid.Parse("2a13551b-059c-4035-b275-8a8cb82bd272"),
+                    Quantity = 3
+                }
+            }
+        };
+    }
+}
